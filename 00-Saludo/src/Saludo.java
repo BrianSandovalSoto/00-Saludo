583e8d7e63dd5874c06b53a1c85c8d8911924e1c
BrianSandovalSoto <brians_19@hotmail.com>
2016-01-15 14:36:48 -0600

    00-Saludo Práctica realizada en clase

---
+++ 00-Saludo/src/Saludo.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+/**
+ * 
+ * @author Brian David Sandoval Soto
+ *
+ */
+public class Saludo {
+
+	public static void main(String[] args) {
+		//JOptionPane.showMessageDialog(null, "Hola Cloud"); // Mensaje Gráfico
+		System.out.println("Mensaje desde la consola");
+	}
+
+}
